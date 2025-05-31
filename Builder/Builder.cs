34e60c6c1a0841ddc34710c895bc4d72d255a61a
valeryiaBerazhnaya <valeryia.berazhnaya@student.ehu.lt>
2025-05-31 12:48:27 +0300

    NewBuilder

---
+++ Builder/Builder.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Builder
+{
+    public static class Builder
+    {
+        public static void Run()
+        {
+            var director = new VehicleDirector();
+
+            var carBuilder = new CarBuilder();
+            var car = director.Construct(carBuilder);
+            Console.WriteLine(car);
+
+            var bikeBuilder = new BikeBuilder();
+            var bike = director.Construct(bikeBuilder);
+            Console.WriteLine(bike);
+        }
+
+        public class Vehicle
+        {
+            public string? Type { get; set; }
+            public string? Engine { get; set; }
+            public int Wheels { get; set; }
+
+            public override string ToString()
+            {
+                return $"[Vehicle] Type: {Type}, Engine: {Engine}, Wheels: {Wheels}";
+            }
+        }
+
+        public interface IVehicleBuilder
+        {
+            void SetType();
+            void SetEngine();
+            void SetWheels();
+            Vehicle GetResult();
+        }
+
+        public class CarBuilder : IVehicleBuilder
+        {
+            private readonly Vehicle _vehicle = new();
+
+            public void SetType() => _vehicle.Type = "Car";
+            public void SetEngine() => _vehicle.Engine = "V6";
+            public void SetWheels() => _vehicle.Wheels = 4;
+            public Vehicle GetResult() => _vehicle;
+        }
+
+        public class BikeBuilder : IVehicleBuilder
+        {
+            private readonly Vehicle _vehicle = new();
+
+            public void SetType() => _vehicle.Type = "Bike";
+            public void SetEngine() => _vehicle.Engine = "Single-cylinder";
+            public void SetWheels() => _vehicle.Wheels = 2;
+            public Vehicle GetResult() => _vehicle;
+        }
+
+        public class VehicleDirector
+        {
+            public Vehicle Construct(IVehicleBuilder builder)
+            {
+                builder.SetType();
+                builder.SetEngine();
+                builder.SetWheels();
+                return builder.GetResult();
+            }
+        }
+    }
+}
--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,33 +1,12 @@
-﻿namespace Builder
+﻿using System;
+
+namespace Builder
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var cocktail = new Drink.Builder()
-                .SetType("Cocktail")
-                .SetAlcohol(true)
-                .SetGlassType("Goblet")
-                .SetDecoration("Lemon")
-                .Build();
-
-            var juice = new Drink.Builder()
-                .SetType("Juice")
-                .SetAlcohol(false)
-                .SetGlassType("Glass")
-                .SetDecoration("Mint")
-                .Build();
-
-            var bloodyMary = new Drink.Builder()
-                .SetType("Cocktail")
-                .SetAlcohol(true)
-                .SetGlassType("Glass")
-                .SetDecoration("Tomato")
-                .Build();
-
-            Console.WriteLine(cocktail);
-            Console.WriteLine(juice);
-            Console.WriteLine(bloodyMary);
+            Builder.Run();
         }
     }
 }
